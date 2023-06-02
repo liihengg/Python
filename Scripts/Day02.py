48b3aa11b75e44023c600eb10b9bd3fad5241c16
liheng02 <liheng@yahaha.com>
2023-06-03 00:46:55 +0800

    day02

---
+++ Scripts/Day02.py
@@ -0,0 +1,21 @@
+﻿# c = 1 + 1j
+# d = 'ddd'
+# e = True
+# 
+# print(type(c))
+# print(type(d))
+# print(type(e))
+# 
+# a = int(input('a = '))
+# b = int(input('b = '))
+# print('%d + %d = %d' % (a, b, a + b))
+# print('%d - %d = %d' % (a, b, a - b))
+# print('%d * %d = %d' % (a, b, a * b))
+# print('%d / %d = %f' % (a, b, a / b))
+# print('%d // %d = %d' % (a, b, a // b))
+# print('%d %% %d = %d' % (a, b, a % b))
+# print('%d ** %d = %d' % (a, b, a ** b))
+
+a = int(input("F = "))
+b = (a - 32) / 1.8
+print(b)
