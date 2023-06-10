98d87a0f1a733666ea141dcf25fc3afd4775525e
liheng02 <liheng@yahaha.com>
2023-06-11 01:56:31 +0800

    update

--- Program/RequestUrl.py
+++ Program/RequestUrl.py
@@ -3,10 +3,21 @@ import time
 import requests
 import os
 from lxml import html
+from enum import Enum
 
-urlContentPath = '../Resource/urlContent.html'
-textureUrl = 'https://meirentu.cc/pic/984846992922.html'
 fileEncoding = 'utf-8'
+
+resourcePth = '../Resource'
+urlContentPath = resourcePth + '/urlContent.html'
+textureUrl = 'https://meirentu.cc/pic/984846992922.html'
+globalInfoTextPath = resourcePth + '/Texture/GlobalText.text'
+
+
+class UrlState(Enum):
+    NotStarted = 1
+    Downloading = 2
+    Done = 3
+    Failed = 4
 
 
 class DownloadHandler(Thread):
@@ -29,7 +40,28 @@ class DownloadHandler(Thread):
             print(resp.text)
 
 
+def CheckUrlState(url):
+    try:
+        with open(globalInfoTextPath, 'a+', fileEncoding) as f:
+            globalInfo = f.read()
+            hasUrl: bool = False
+            for k, v in globalInfo:
+                if k == url:
+                    print(k)
+    except FileNotFoundError:
+        print()
+
+
+def CreateInfoFile(htmlTree):
+    title = htmlTree.xpath('body//div[@class="item_title"]/h1/text()')
+    print(title[0])
+    info = htmlTree.xpath('body//article/p/text()')
+    for a in info:
+        print(a)
+
+
 def main():
+    CheckUrlState('12345678')
     # if os.path.exists('../Resource/Texture/1'):
     #     print('文件已存在')
     # else:
@@ -42,17 +74,18 @@ def main():
     #     urlContent = resp.text
 
     # DownloadHandler('https://cdn2.mmdb.cc/file/20230610/984846992922/04080472.jpg').start()
-    with open(urlContentPath, 'r', encoding=fileEncoding) as f:
-        # with open('../Resource/testHtml.html', 'r', encoding=fileEncoding) as f:
-        text = f.read()
-        tree = html.fromstring(text)
-        # article = tree.xpath('head/title[1]/text()')
-        article = tree.xpath('head/link[1]')
-        for a in article:
-            print(a.attrib)
-        # article = tree.xpath('head/meta[1]')
-        # for a in article:
-        #    print(a.attrib)
+    # with open(urlContentPath, 'r', encoding=fileEncoding) as f:
+    # with open('../Resource/testHtml.html', 'r', encoding=fileEncoding) as f:
+
+    # text = f.read()
+    # tree = html.fromstring(text)
+    # CreateInfoFile(tree)
+
+    # article = tree.xpath('head/title[1]/text()')
+    # title = tree.xpath('body/div[2]/div[1]/div[1]/div[1]')
+    # article = tree.xpath('head/meta[1]')
+    # for a in article:
+    #    print(a.attrib)
 
 
 if __name__ == '__main__':
